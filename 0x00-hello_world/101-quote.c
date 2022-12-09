c0c25404fa0012492a45495c9372dc3c05e4ec9c
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-12-09 10:24:25 -0800

    gooo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fprintf(stderr, str);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "\n");
 return (1);
 }
