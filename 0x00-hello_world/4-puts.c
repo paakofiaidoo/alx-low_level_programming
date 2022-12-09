2026b45647f47306b4b633b6daf7996a57432ac0
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-12-09 07:03:53 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints exactly string
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Print the string using the printf function.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-  // Print the string using the printf function
   printf("with proper grammar, but the outcome is a piece of art,");
   return (0);
 }
